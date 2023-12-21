aba79c86c9979257e54a57bd4e58b929297347b8
Joseph Chong <jschong1@cps.edu>
2023-12-21 12:36:18 -0600

    fnished a3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -8,15 +8,40 @@ data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
 x = data[["miles","age"]].values
 y = data["Price"].values
 
-#split the data into training and testing data
 
-#create linear regression model
+# separates the data innto training and testing sets
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 
-#Find and print the coefficients, intercept, and r squared values. 
-#Each should be rounded to two decimal places. 
+# # reshape the xtrain data into a 2D array
+# xtrain = xtrain.reshape(-1, 1)
 
+# create the linear regression model using the training data
+model = LinearRegression().fit(xtrain, ytrain)
 
-#Loop through the data and print out the predicted prices and the 
-#actual prices
-print("***************")
-print("Testing Results")
+# get the coef_, intercept_ valuesm and r^2 values
+# use float() to turn the arrays into a single float value
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
+
+# print out the linear equation and r^2 value
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {intercept}")
+print("R Squared value:", r_squared)
+
+'''
+**********TEST THE MODEL**********
+'''
+
+# get the predicted y values for the xtest values - returns an array of the results
+predict = model.predict(xtest)
+# round the value in the np array to 2 decimal places
+predict = np.around(predict, 2)
+print(predict)
+
+# compare the actual and predicted values
+print("\nTesting Multivariable Model with Testing Data:")
+for index in range(len(xtest)):
+    actual = ytest[index] # gets the actual y value from the ytest dataset
+    predicted_y = predict[index] # gets the predicted y value from the predict variable
+    x_coord = xtest[index] # gets the x value from the xtest dataset
+    print(f"miles(000): {x_coord[0]} age: {x_coord[1]} Actual: {actual} Predicted: {predicted_y}")
